5a75a46a900fe630e531c302ab056d765ed28700
Mehdi JL <jlmehdi01@gmail.com>
2021-11-10 14:44:26 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Counter from "./components/counter";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+import GalleryDetails from "./components/GalleryDetails";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <nav className="navbar navbar-expand navbar-brand m-2">
+                <ul className="navbar-nav">
+                    <li><Link className="nav-link" to={"/home"}>Home</Link></li>
+                    <li><Link className="nav-link" to={"/counter"}>Counter</Link></li>
+                    <li><Link className="nav-link" to={"/about"}>About</Link></li>
+                    <li><Link className="nav-link" to={"/gallery"}>Gallery</Link></li>
+                </ul>
+            </nav>
+            <div className="container">
+                <Switch>
+                    <Route path="/home"/>
+                    <Route path="/counter" component={Counter}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/gallery" component={Gallery}/>
+                    <Route path="/details/:id" component={GalleryDetails}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
